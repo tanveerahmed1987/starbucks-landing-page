78f18394ebea80ffa2d75e5416f715d405cacb88
Tanveer Ahmed <tanveer.ahmed.contractor@move.com>
2021-02-08 01:10:21 +0530

    Starbucks landing page desktop version

---
+++ script.js
@@ -0,0 +1,8 @@
+function imageSlider(image) {
+  document.querySelector(".starbucks").src = image;
+}
+
+function changeCircleColor(color) {
+  const circle = document.querySelector(".circle");
+  circle.style.background = color;
+}
